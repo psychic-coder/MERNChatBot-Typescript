4d265f824c07cfa9d66a66d97cde3397678f8c62
psychic-coder <rohitganguly450@gmail.com>
2024-08-07 13:52:18 +0530

    made the signup route

--- Backend/src/controllers/user.controllers.ts
+++ Backend/src/controllers/user.controllers.ts
@@ -1,24 +1,48 @@
 import { NextFunction, Request, Response } from "express";
-import User from "../models/User.js"
+import User from "../models/User.js";
+import bcryptjs from "bcryptjs";
 
-export const getAllUsers=async (req:Request,res:Response,next:NextFunction)=>{
-        try {
-            const users=await User.find();
-            return res.status(200).json({
-                message:"OK",
-                users
-            })
-        } catch (error) {
-            if (error instanceof Error) {
-                return res.status(200).json({
-                    message: "Error",
-                    cause: error.message
-                });
-            } else {
-                return res.status(200).json({
-                    message: "Error",
-                    cause: "Unknown error"
-                });
-            }
-        }
-}
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({
+      message: "OK",
+      users,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(200).json({
+        message: "Error",
+        cause: error.message,
+      });
+    } else {
+      return res.status(200).json({
+        message: "Error",
+        cause: "Unknown error",
+      });
+    }
+  }
+};
+
+export const userSignUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    return res.status(200).json({ message: "OK", id:user._id.toString() });
+  } catch (error) {
+    return res.status(200).json({
+      message: "Error",
+      cause: "Unknown error",
+    });
+  }
+};
--- Backend/src/routes/user-routes.ts
+++ Backend/src/routes/user-routes.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { getAllUsers } from "../controllers/user.controllers.js";
+import { getAllUsers, userSignUp } from "../controllers/user.controllers.js";
 
 const userRoutes=express.Router();
 
 userRoutes.get("/",getAllUsers);
+userRoutes.post("/signup",userSignUp);
 
 
 export default userRoutes;
