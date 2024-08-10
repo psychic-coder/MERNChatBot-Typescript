12338a9f4e7167a5fef95f5e96901ea13523bf71
psychic-coder <rohitganguly450@gmail.com>
2024-08-11 02:13:22 +0530

    in the process of making the chat controllers

--- Backend/src/controllers/chat.controllers.ts
+++ Backend/src/controllers/chat.controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+// import { OpenAIApi, ChatCompletionRequestMessage } from "openai";
 
 export const generateChatCompletion = async (
   req: Request,
@@ -11,7 +12,15 @@ export const generateChatCompletion = async (
     if(!user){
         return res.status(401).json({message:"User is not registered or token malfunctions"});
     }
-    
+    //chats of the user
+    //send all the chats with one to openai api
 
+    const chats = user.chats.map(({ role, content }) => ({
+        role,
+        content,
+      })) as ChatCompletionRequestMessage[];
+      chats.push({ content: message, role: "user" });
+      user.chats.push({ content: message, role: "user" });
+  
 
 };
