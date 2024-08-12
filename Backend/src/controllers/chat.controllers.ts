9607d74651ca972ab824cd2ef3ac6ca84e11b687
psychic-coder <rohitganguly450@gmail.com>
2024-08-12 07:20:56 +0530

    made the backend controllers

--- Backend/src/controllers/chat.controllers.ts
+++ Backend/src/controllers/chat.controllers.ts
@@ -1,6 +1,12 @@
 import OpenAI from "openai";
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User.js";
+import MistralClient from '@mistralai/mistralai';
+
+
+
+
+
  // Assuming you have a User model in the models directory
 
 export const generateChatCompletion = async (
@@ -8,6 +14,11 @@ export const generateChatCompletion = async (
   res: Response,
   next: NextFunction
 ) => {
+
+// const apiKey = process.env.MISTRAL_API_KEY;
+// const client = new MistralClient(apiKey);
+
+
   try {
     const openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY!, // Use your environment variable or hardcode the API key here
@@ -46,5 +57,51 @@ export const generateChatCompletion = async (
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+
+export const sendChatsToUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //user token check
+    const user = await User.findById(res.locals.jwtData.id);
+    if (!user) {
+      return res.status(401).send("User not registered OR Token malfunctioned");
+    }
+    if (user._id.toString() !== res.locals.jwtData.id) {
+      return res.status(401).send("Permissions didn't match");
+    }
+    return res.status(200).json({ message: "OK", chats: user.chats });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause:"Error in sendChatsToUser" });
+  }
+};
+
+export const deleteChats = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //user token check
+    const user = await User.findById(res.locals.jwtData.id);
+    if (!user) {
+      return res.status(401).send("User not registered OR Token malfunctioned");
+    }
+    if (user._id.toString() !== res.locals.jwtData.id) {
+      return res.status(401).send("Permissions didn't match");
+    }
+    //@ts-ignore
+    user.chats = [];
+    await user.save();
+    return res.status(200).json({ message: "OK" });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: "error in deleteChats"});
   }
 };
