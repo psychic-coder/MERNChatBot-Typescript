15fd8e51baa9b453dc5de10e5f6e75a24cb36ede
psychic-coder <rohitganguly450@gmail.com>
2024-08-12 12:31:38 +0530

    made the ui of the chat page

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -5,7 +5,7 @@ import chatRoutes from "./chat-routes.js";
 const appRouter=express.Router();
 
 appRouter.use('/user',userRoutes);
-appRouter.use("/chats",chatRoutes)
+appRouter.use("/chat",chatRoutes)
 
 
 export default appRouter;
