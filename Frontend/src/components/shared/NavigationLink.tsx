dfecfe60745272cd1b75bdaa2747aed6d11ad936
psychic-coder <rohitganguly450@gmail.com>
2024-08-13 16:21:27 +0530

    made the signup page

--- Frontend/src/components/shared/NavigationLink.tsx
+++ Frontend/src/components/shared/NavigationLink.tsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 
-type Props={
-  to:string;
-  bg:string;
-  text:string;
-  textColor:string;
-  onClick?:()=>Promise<void>
-}
-const NavigationLink = (props:Props) => {
+type Props = {
+  to: string;
+  bg: string;
+  text: string;
+  textColor: string;
+  onClick?: () => Promise<void>;
+};
+const NavigationLink = (props: Props) => {
   return (
-   <Link className="nav-link" to={props.to} style={{background:props.bg,color:props.textColor}}>
-    {props.text}
-   </Link>
-  )
-}
+    <Link
+      onClick={props.onClick}
+      className="nav-link"
+      to={props.to}
+      style={{ background: props.bg, color: props.textColor }}
+    >
+      {props.text}
+    </Link>
+  );
+};
 
-export default NavigationLink
+export default NavigationLink;
